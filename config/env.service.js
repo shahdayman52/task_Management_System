bf11a069dbda9f75cb39c3baeb17530a500a848c
shahdayman52 <shahdayman5757@gmail.com>
2026-01-30 01:55:52 +0200

    jwt secret key

--- config/env.service.js
+++ config/env.service.js
@@ -4,4 +4,6 @@ dotenv.config({ path: "./config/.env" });
 const databaseName = process.env.DATABASE_NAME;
 const databaseUser = process.env.DATABASE_USER;
 const databasePassword = process.env.DATABASE_PASSWORD;
-export { databaseName, databaseUser, databasePassword };
+const jwtSecret = process.env.JWT_SECRET;
+
+export { databaseName, databaseUser, databasePassword, jwtSecret };
