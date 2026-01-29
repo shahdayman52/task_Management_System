2be1b601ca08a5d5a890b770879696a4a01712a0
shahdayman52 <shahdayman5757@gmail.com>
2026-01-30 01:56:41 +0200

    app:add user router to the bootstrap

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { databaseConnection, databaseSync } from "./database/connection.js";
 import "./database/model/relation.js";
+import userRouter from "./modules/user/user.controller.js";
+
 export const bootstrap = async () => {
   const app = express();
   await databaseConnection();
   await databaseSync();
+  app.use(express.json());
+
+  app.use("/user", userRouter);
 
   app.use((error, req, res, next) => {
     res.json({
@@ -13,7 +18,6 @@ export const bootstrap = async () => {
       stack: error.stack,
     });
   });
-  app.use(express.json());
   app.listen(3000, () => {
     console.log("Running on port 3000");
   });
