a62e6b27b21dd4c9999e23cada7dbcdc1080d4a8
shahdayman52 <shahdayman5757@gmail.com>
2026-01-31 15:12:37 +0200

    Ready for Railway deployment

--- config/env.service.js
+++ config/env.service.js
@@ -5,5 +5,8 @@ const databaseName = process.env.DATABASE_NAME;
 const databaseUser = process.env.DATABASE_USER;
 const databasePassword = process.env.DATABASE_PASSWORD;
 const jwtSecret = process.env.JWT_SECRET;
+const port = process.env.DATABASE_PORT;
 
-export { databaseName, databaseUser, databasePassword, jwtSecret };
+const host = process.env.DATABASE_HOST;
+
+export { databaseName, databaseUser, databasePassword, jwtSecret, port, host};
